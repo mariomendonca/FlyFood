340bcdaeebd87a19780430955107209f047cd87c
Mario Mendonca <mariomeendonca00@gmail.com>
2021-02-03 08:45:54 -0300

    shortest distance function

--- flyfood.py
+++ flyfood.py
@@ -12,7 +12,7 @@ def get_all_points(matrix):
   points = []
   for line in matrix:
     for x in line:
-      if (x != '0') and (x != 'R'):
+      if (x != '0'):
         point_tuple = (x, matrix.index(line), line.index(x))
         points.append(point_tuple)
 
@@ -22,10 +22,56 @@ def all_routes(matrix):
 
   return
 
-def shortest_distance(array, points):
+rotas = [ 'ABCD', 'DCBA', 'DBAC']
+# rotas = ['ABCD', 'ACBD']
+
+def shortest_distance(routes, points):
+  melhor_rota_distancia = 0
+  melhor_rota = None
+
+  # cada rota
+  for route in routes:
   
-  return
+    valor_total = 0 
+    # cada ponto
+    ponto_ordernados = []
+    for point in route:
+      # print(route, point)
+    
+      for x in points:
+        if point == x[0]:
+          ponto_ordernados.append(x)
+
+    # ponto de partida e chegada (estabelecimento)
+    for point in points:
+      if point[0] == 'R':
+        ponto_ordernados.insert(0, point)
+        ponto_ordernados.append(point)
+
+    # print(ponto_ordernados)
+    for i in range(len(ponto_ordernados) - 1):
+      soma1 = abs(ponto_ordernados[i][1] - ponto_ordernados[i + 1][1])
+      soma2 = abs(ponto_ordernados[i][2] - ponto_ordernados[i + 1][2])
+      distancia = soma1 + soma2
+
+      valor_total += distancia
+    print(valor_total)
+
+    if melhor_rota_distancia == 0:
+      melhor_rota_distancia = valor_total
+    if valor_total <= melhor_rota_distancia:
+      melhor_rota_distancia = valor_total
+      melhor_rota = route
+
+  return melhor_rota, melhor_rota_distancia
 
 
-print(get_all_points(lista)) 
+
+# fazer a msm logica de colocar numa lista e dps soma lo
+
+
+
+# shortest_distance(rotas, get_all_points(lista))
+print(shortest_distance(rotas, get_all_points(lista)))
+# print(get_all_points(lista)) 
 matrix.close()
--- shortest_distance.py
+++ shortest_distance.py
@@ -5,15 +5,7 @@ def menor_distancia(matriz):
       if x != '0':
         pontos.append(matriz.index(line))
         pontos.append(line.index(x))
-  # if pontos[0] > pontos[2]:
-  #   soma1 = pontos[0] - pontos[2]
-  # else:
-  #   soma1 = pontos[2] - pontos[0]
-    
-  # if pontos[1] > pontos[3]:
-  #   soma2 = pontos[1] - pontos[3]
-  # else:
-  #   soma2 = pontos[3] - pontos[1]
+        
   soma1 = abs(pontos[0] - pontos[2])
   soma2 = abs(pontos[1] - pontos[3])
   soma_total = soma1 + soma2
