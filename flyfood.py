9e24a04033c229b09d594199a54baf8a36699136
Mario Mendonca <mariomeendonca00@gmail.com>
2021-02-04 14:17:12 -0300

    finished

--- flyfood.py
+++ flyfood.py
@@ -1,47 +1,71 @@
-lista = []
-matrix = open('matrix.txt', 'r')
-for line in matrix: 
-  linha = []
-  for x in line:
-    if (x != ' ') and (x != '\n'):
-      linha.append(x)
-  lista.append(linha)
+def get_matrix(matrix_txt):
+  lista = []
+  for line in matrix: 
+    linha = []
+    for x in line:
+      if (x != ' ') and (x != '\n'):
+        linha.append(x)
+    lista.append(linha)
+  return lista
 
+
+def get_all_points_and_indexes(matrix):
+  array = []
+  for line in matrix:
+    for x in line:
+      if (x != '0'):
+        point_tuple = (x, matrix.index(line), line.index(x))
+        array.append(point_tuple)
+  return array 
 
 def get_all_points(matrix):
   points = []
   for line in matrix:
     for x in line:
-      if (x != '0'):
-        point_tuple = (x, matrix.index(line), line.index(x))
-        points.append(point_tuple)
-
+      if (x != '0') and (x != 'R'):
+        points.append(x)
+  
   return points
 
-def all_routes(matrix):
 
-  return
+def permutation(points):
+  if len(points) == 0:
+    return []
+  elif len(points) == 1:
+    return [points]
+  else:
+    lista = []
+    for i in range(len(points)):
+      x = points[i]
+      xs = points[:i] + points[i + 1:]
+      for p in permutation(xs):
+        lista.append([x] + p)
 
-rotas = [ 'ABCD', 'DCBA', 'DBAC']
-# rotas = ['ABCD', 'ACBD']
+  return lista
+
+
+def all_routes(points):
+  routes = []
+  for x in permutation(points):
+    string = ''.join(x)
+    routes.append(string)
+
+  return routes
+
 
 def shortest_distance(routes, points):
   melhor_rota_distancia = 0
   melhor_rota = None
-
   # cada rota
   for route in routes:
-  
     valor_total = 0 
     # cada ponto
     ponto_ordernados = []
     for point in route:
       # print(route, point)
-    
       for x in points:
         if point == x[0]:
           ponto_ordernados.append(x)
-
     # ponto de partida e chegada (estabelecimento)
     for point in points:
       if point[0] == 'R':
@@ -53,7 +77,6 @@ def shortest_distance(routes, points):
       soma1 = abs(ponto_ordernados[i][1] - ponto_ordernados[i + 1][1])
       soma2 = abs(ponto_ordernados[i][2] - ponto_ordernados[i + 1][2])
       distancia = soma1 + soma2
-
       valor_total += distancia
     print(valor_total)
 
@@ -66,12 +89,13 @@ def shortest_distance(routes, points):
   return melhor_rota, melhor_rota_distancia
 
 
+matrix = open('matrix.txt', 'r')
+matriz = get_matrix(matrix)
+tuple_points = get_all_points_and_indexes(matriz)
+points = get_all_points(matriz)
+routes = all_routes(points)
+menor_distancia = shortest_distance(routes, tuple_points)
 
-# fazer a msm logica de colocar numa lista e dps soma lo
+print(menor_distancia)
 
-
-
-# shortest_distance(rotas, get_all_points(lista))
-print(shortest_distance(rotas, get_all_points(lista)))
-# print(get_all_points(lista)) 
 matrix.close()
